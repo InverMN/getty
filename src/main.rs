b6fc22ad53ac1703f12b7b4ef1a0c8b2a016a9a5
Inver <jankowskipawelxd@gmail.com>
2020-12-21 09:36:44 +0100

    Rename names to specify database

--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,28 @@ use std::time::Duration;
 
 use aerospike::{Client, ClientPolicy};
 
-static mut DATABASE_CLIENT: Option<Arc<aerospike::Client>> = None;
+static mut AEROSPIKE_CLIENT: Option<Arc<aerospike::Client>> = None;
 
 fn main() {
   connect_database();
   rocket::ignite().mount("/", routes![index]).launch();
 }
 
-fn try_connecting_database_once() {
+fn try_connecting_aerospike_database_once() {
   let client_policy = ClientPolicy::default();
   let url = "127.0.0.1:3000";
   Client::new(&client_policy, &url).and_then(|i| {
     unsafe {
-      Ok(DATABASE_CLIENT = Some(Arc::new(i)))
+      Ok(AEROSPIKE_CLIENT = Some(Arc::new(i)))
     }
   }).ok();
 }
 
-fn connect_database() {
+fn connect_aerospike_database() {
   try_connecting_database_once();
 
   unsafe {
-    while DATABASE_CLIENT.is_none() {
+    while AEROSPIKE_CLIENT.is_none() {
       println!("Waiting 60 seconds before reconnecting to database...");
       thread::sleep(Duration::from_secs(60));
       try_connecting_database_once();
@@ -43,3 +43,6 @@ fn connect_database() {
 fn index() -> &'static str {
   "Welcome to Get JWT auth server"
 }
+
+// #[post["/api/v1/"]]
+// fn register() -
