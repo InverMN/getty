c0cbc10c895514daff562c487da1592a85eef932
Inver <jankowskipawelxd@gmail.com>
2020-12-21 09:27:01 +0100

    Add connecting to database

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,44 @@
 
 #[macro_use] extern crate rocket;
 
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
+
+use aerospike::{Client, ClientPolicy};
+
+static mut DATABASE_CLIENT: Option<Arc<aerospike::Client>> = None;
+
 #[get("/")]
 fn index() -> &'static str {
   "Welcome to Get JWT auth server"
 }
 
+fn try_connecting_database_once() {
+  let client_policy = ClientPolicy::default();
+  let url = "127.0.0.1:3000";
+  Client::new(&client_policy, &url).and_then(|i| {
+    unsafe {
+      Ok(DATABASE_CLIENT = Some(Arc::new(i)))
+    }
+  }).ok();
+}
+
+fn connect_database() {
+  try_connecting_database_once();
+
+  unsafe {
+    while DATABASE_CLIENT.is_none() {
+      println!("Waiting 60 seconds before reconnecting to database...");
+      thread::sleep(Duration::from_secs(60));
+      try_connecting_database_once();
+    }
+  }
+
+  println!("Connected to database")
+}
+
 fn main() {
+  connect_database();
   rocket::ignite().mount("/", routes![index]).launch();
 }
