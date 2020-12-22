0617ed68cf70cededbe63d83d0d205274a9ee1b0
Inver <jankowskipawelxd@gmail.com>
2020-12-22 18:20:10 +0100

    Delete unwanted /login and /register

--- src/json.rs
+++ src/json.rs
@@ -1,19 +1,6 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize)]
-pub struct RegisterData {
-  pub email: String,
-  pub username: String,
-  pub password: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct LoginData {
-  pub email: String,
-  pub password: String,
-}
-
-#[derive(Debug, Deserialize)]
 pub struct RefreshTokenData {
   pub refresh_token: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,27 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
 
-mod aerospike_database;
-mod mongo_database;
+mod database;
 mod json;
 mod tokens;
 
 use regex::Regex;
 use rocket::State;
 use rocket_contrib::json::Json;
-use mongodb::{
-  bson::{doc, Bson},
-  sync::{Client, Collection},
-};
 use bcrypt::{hash, verify};
 use tokens::{sign_refresh_token, sign_access_token, verify_refresh_token};
 
 fn main() {
-  let aerospike_client = aerospike_database::connect();
-  let mongo_client = mongo_database::connect();
-
   rocket::ignite()
-    .manage(mongo_client)
-    .manage(aerospike_client)
+    .manage(database::connect())
     .mount("/", routes![index])
-    .mount("/api/v1/", routes![register, login, refresh, logout])
+    .mount("/api/v1/", routes![refresh, logout])
     .launch();
 }
 
 #[get("/")]
 fn index() -> &'static str {
   "Welcome to Getty JWT auth server"
-}
-
-#[post("/register", data = "<register_data>")]
-fn register(register_data: Json<json::RegisterData>, mongo_client: State<Client>) -> Json<json::AuthTokens> {
-  let user_collection = mongo_database::user_collection(&mongo_client);
-
-  let hashed_password = hash(&register_data.password, 8).unwrap();
-
-  let register_data_document = doc! {
-    "email": &register_data.email,
-    "username": &register_data.username,
-    "hashedPassword": hashed_password,
-  };
-
-  let insertion_result = user_collection.insert_one(register_data_document, None).unwrap();
-  let user_id_object = insertion_result.inserted_id.as_object_id().unwrap();
-  let user_id = user_id_object.to_hex();
-
-  let refresh_token = sign_refresh_token(&user_id);
-  let access_token = sign_access_token(&user_id);
-
-  let auth_tokens = json::AuthTokens { refresh_token, access_token };
-
-  Json(auth_tokens)
-}
-
-#[post("/login", data = "<login_data>")]
-fn login(login_data: Json<json::LoginData>, mongo_client: State<Client>) -> Json<json::AuthTokens> {
-  let user_collection = mongo_database::user_collection(&mongo_client);
-
-  let search_document = doc! {
-    "email": &login_data.email
-  };
-
-  let user_document = user_collection.find_one(Some(search_document), None).unwrap().unwrap();
-  let hashed_password = user_document.get_str("hashedPassword").unwrap();
-  
-  if verify(&login_data.password, hashed_password).unwrap() {
-    println!("Passwords are the same");
-  } else {
-    println!("Passwords are different");
-  }
-
-  let user_id_object = user_document.get_object_id("_id").unwrap();
-  let user_id = user_id_object.to_hex();
-  
-  let refresh_token = sign_refresh_token(&user_id);
-  let access_token = sign_access_token(&user_id);
-
-  let auth_tokens = json::AuthTokens { refresh_token, access_token };
-
-  Json(auth_tokens)
 }
 
 #[post("/refresh", data="<refresh_token_data>")]
