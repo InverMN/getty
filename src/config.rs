fe30805861bbb3e49687c134e1f567e2d289ec57
Inver <jankowskipawelxd@gmail.com>
2020-12-24 19:54:21 +0100

    Load config from env variables

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,14 @@
+use std::env::var;
+
 pub fn load_config() -> Config {
-  Config::default()
+  Config {
+    secret: var("GETTY_SECRET").unwrap_or("REMEMBER TO CHANGE SECRET VIA ENV VARIABLES!".to_owned()),
+    refresh_token_exp: (var("GETTY_REFRESH_TOKEN_EXP").unwrap_or((10 * 24 * 60 * 60 * 1000).to_string())).parse::<u128>().unwrap(), // 10 days
+    access_token_exp: (var("GETTY_ACCESS_TOKEN_EXP").unwrap_or((10 * 60 * 1000).to_string())).parse::<u128>().unwrap(), // 10 minutes
+    database_host: var("GETTY_DATABASE_HOST").unwrap_or("localhost:3000".to_owned()),
+    database_namespace: var("GETTY_DATABASE_NAMESPACE").unwrap_or("getty-auth".to_owned()),
+    database_set: var("GETTY_DATABASE_SET").unwrap_or("tokens".to_string()),
+  }
 }
 
 pub struct Config {
@@ -9,17 +18,4 @@ pub struct Config {
   pub database_host: String,
   pub database_namespace: String,
   pub database_set: String,
-}
-
-impl Config {
-  fn default() -> Config {
-    Config {
-      secret: "REMEMBER TO CHANGE SECRET VIA ENV VARIABLES!".to_owned(),
-      refresh_token_exp: 10 * 24 * 60 * 60 * 1000, // 10 days
-      access_token_exp: 10 * 60 * 1000, // 10 minutes
-      database_host: "localhost:3000".to_owned(),
-      database_namespace: "getty-auth".to_owned(),
-      database_set: "tokens".to_string(),
-    }
-  }
 }
