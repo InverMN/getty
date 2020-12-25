4f359b383fe36237ea58f1388290a50feba79792
Inver <jankowskipawelxd@gmail.com>
2020-12-25 12:59:40 +0100

    Call function when bidning string to config

--- src/config.rs
+++ src/config.rs
@@ -2,12 +2,12 @@ use std::env::var;
 
 pub fn load_config() -> Config {
   Config {
-    secret: var("GETTY_SECRET").unwrap_or("REMEMBER TO CHANGE SECRET VIA ENV VARIABLES!".to_owned()),
-    refresh_token_exp: (var("GETTY_REFRESH_TOKEN_EXP").unwrap_or((10 * 24 * 60 * 60 * 1000).to_string())).parse::<u128>().unwrap(), // 10 days
-    access_token_exp: (var("GETTY_ACCESS_TOKEN_EXP").unwrap_or((10 * 60 * 1000).to_string())).parse::<u128>().unwrap(), // 10 minutes
-    database_host: var("GETTY_DATABASE_HOST").unwrap_or("localhost:3000".to_owned()),
-    database_namespace: var("GETTY_DATABASE_NAMESPACE").unwrap_or("getty-auth".to_owned()),
-    database_set: var("GETTY_DATABASE_SET").unwrap_or("tokens".to_string()),
+    secret: var("GETTY_SECRET").unwrap_or_else(|_| "REMEMBER TO CHANGE SECRET VIA ENV VARIABLES!".to_string()),
+    refresh_token_exp: (var("GETTY_REFRESH_TOKEN_EXP").unwrap_or_else(|_| (10 * 24 * 60 * 60 * 1000).to_string())).parse::<u128>().unwrap(), // 10 days
+    access_token_exp: (var("GETTY_ACCESS_TOKEN_EXP").unwrap_or_else(|_| (10 * 60 * 1000).to_string())).parse::<u128>().unwrap(), // 10 minutes
+    database_host: var("GETTY_DATABASE_HOST").unwrap_or_else(|_| "localhost:3000".to_string()),
+    database_namespace: var("GETTY_DATABASE_NAMESPACE").unwrap_or_else(|_| "getty-auth".to_string()),
+    database_set: var("GETTY_DATABASE_SET").unwrap_or_else(|_| "tokens".to_string()),
   }
 }
 
