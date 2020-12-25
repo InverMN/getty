d8be84723c59da72ea3edd14aa37574199f634b1
Inver <jankowskipawelxd@gmail.com>
2020-12-25 13:09:12 +0100

    Create standing alone module for routes

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,12 @@ mod database;
 mod structs;
 mod tokens;
 mod config;
+mod routes;
 
-use rocket::State;
-use rocket_contrib::json::Json;
-use tokens::{sign_refresh_token, sign_access_token, verify_refresh_token};
-use database::{save_token, used_token};
-use rocket::http::Status;
-use structs::{AuthTokens, RefreshTokenData};
-use aerospike::Client;
-use config::{load_config, Config};
+use config::load_config;
+use routes::static_rocket_route_info_for_logout;
+use routes::static_rocket_route_info_for_refresh;
+use routes::static_rocket_route_info_for_index;
 
 fn main() {
   let config = load_config();
@@ -24,35 +21,4 @@ fn main() {
     .mount("/", routes![index])
     .mount("/api/v1/", routes![refresh, logout])
     .launch();
-}
-
-#[get("/")]
-fn index() -> &'static str {
-  "JWT auth server"
-}
-
-#[post("/refresh", data="<refresh_token_data>")]
-fn refresh(refresh_token_data: Json<RefreshTokenData>, client: State<Client>, config: State<Config>) -> Result<Json<AuthTokens>, Status> {
-  if used_token(&client, &refresh_token_data.refresh_token, &config).unwrap() {
-    return Err(Status::Unauthorized);
-  }
-  let user_id = verify_refresh_token(&refresh_token_data.refresh_token, &config).unwrap();
-
-  save_token(&client, &refresh_token_data.refresh_token, &config);
-
-  let refresh_token = sign_refresh_token(&user_id, &config);
-  let access_token = sign_access_token(&user_id, &config); 
-
-  let auth_tokens = AuthTokens {
-    refresh_token,
-    access_token,
-  };
-
-  Ok(Json(auth_tokens))
-}
-
-#[post("/logout", data="<refresh_token_data>")]
-fn logout(refresh_token_data: Json<RefreshTokenData>, client: State<Client>, config: State<Config>) -> Status {
-  save_token(&client, &refresh_token_data.refresh_token, &config);
-  Status::Ok
 }
---
+++ src/routes.rs
@@ -0,0 +1,44 @@
+use crate::database;
+use crate::structs;
+use crate::tokens;
+use crate::config;
+
+use rocket::State;
+use rocket_contrib::json::Json;
+use tokens::{sign_refresh_token, sign_access_token, verify_refresh_token};
+use database::{save_token, used_token};
+use rocket::http::Status;
+use structs::{AuthTokens, RefreshTokenData};
+use aerospike::Client;
+use config::Config;
+
+#[get("/")]
+pub fn index() -> &'static str {
+  "JWT auth server"
+}
+
+#[post("/refresh", data="<refresh_token_data>")]
+pub fn refresh(refresh_token_data: Json<RefreshTokenData>, client: State<Client>, config: State<Config>) -> Result<Json<AuthTokens>, Status> {
+  if used_token(&client, &refresh_token_data.refresh_token, &config).unwrap() {
+    return Err(Status::Unauthorized);
+  }
+  let user_id = verify_refresh_token(&refresh_token_data.refresh_token, &config).unwrap();
+
+  save_token(&client, &refresh_token_data.refresh_token, &config);
+
+  let refresh_token = sign_refresh_token(&user_id, &config);
+  let access_token = sign_access_token(&user_id, &config); 
+
+  let auth_tokens = AuthTokens {
+    refresh_token,
+    access_token,
+  };
+
+  Ok(Json(auth_tokens))
+}
+
+#[post("/logout", data="<refresh_token_data>")]
+pub fn logout(refresh_token_data: Json<RefreshTokenData>, client: State<Client>, config: State<Config>) -> Status {
+  save_token(&client, &refresh_token_data.refresh_token, &config);
+  Status::Ok
+}
